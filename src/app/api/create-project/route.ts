d7012451e0835b0a9eb8af3da2f70408a88f5832
longj724 <justinlong596@gmail.com>
2024-04-04 23:49:09 -0400

    Save project system prompt in db

--- src/app/api/create-project/route.ts
+++ src/app/api/create-project/route.ts
@@ -7,12 +7,27 @@ export async function POST(request: Request) {
   try {
     const { name, programmingLanguages, packages, context } =
       await request.json();
+
+    let systemPrompt = `
+    You are an AI assistant that is helping a software engineer with programming tasks. Engineers will ask you questions 
+    and you will provide answers to them and write code for them when needed. The engineer might provide you with
+    information about the project they are working on or rules about how they want to you to answer their questions.
+
+    Here is some background information about the project:
+
+    ${programmingLanguages ? `Programming languages used: ${programmingLanguages}` : ""}
+    ${packages ? `Packages used: ${packages}` : ""}
+    ${context ? `Context: ${context}` : ""}
+    `;
+    systemPrompt = systemPrompt.replace(/r?\n/g, "");
+
     const projectResponse = await db.projects.create({
       data: {
         name,
         programming_languages: programmingLanguages,
         packages,
         context,
+        system_prompt: systemPrompt,
       },
     });
 
