fe2b670e7b7205606fef7471d493c615312737d2
longj724 <justinlong596@gmail.com>
2024-04-19 22:56:51 -0400

    Add send button

--- src/app/_components/MessagePromptsMenu.tsx
+++ src/app/_components/MessagePromptsMenu.tsx
@@ -1,5 +1,5 @@
 // External Dependencies
-import { ChevronUp } from "lucide-react";
+import { ChevronUp, Send } from "lucide-react";
 
 // Relative Dependencies
 import {
@@ -19,8 +19,7 @@ type Props = {
 };
 
 const MessagePromptsMenu = ({ userInput }: Props) => {
-  const handleSendPrompt = (e: Event, prompt: string) => {
-    e.preventDefault();
+  const handleSendPrompt = (prompt: string) => {
     console.log("prompt is", prompt);
   };
 
@@ -39,19 +38,29 @@ const MessagePromptsMenu = ({ userInput }: Props) => {
         <DropdownMenuContent align="end">
           <DropdownMenuItem
             className="hover:cursor-pointer"
-            onSelect={(e) => handleSendPrompt(e, "Explain this error")}
+            onSelect={(e) => e.preventDefault()}
           >
             <div className="flex w-full flex-row items-center gap-2">
               <p>Explain this error</p>
+              <Send
+                className="hover:pointer rounded-sm p-[3px] hover:bg-gray-500"
+                onClick={() => handleSendPrompt("Explain this error")}
+                size={22}
+              />
               <EditPromptModal existingPrompt={"Explain this error"} />
             </div>
           </DropdownMenuItem>
           <DropdownMenuItem
             className="hover:cursor-pointer"
-            onSelect={(e) => handleSendPrompt(e, "Explain these lines of code")}
+            onSelect={(e) => e.preventDefault()}
           >
             <div className="flex w-full flex-row items-center gap-2">
               <p>Explain these lines of code</p>
+              <Send
+                className="hover:pointer rounded-sm p-[3px] hover:bg-gray-500"
+                onClick={() => handleSendPrompt("Explain these lines of code")}
+                size={22}
+              />
               <EditPromptModal existingPrompt={"Explain these lines of code"} />
             </div>
           </DropdownMenuItem>
